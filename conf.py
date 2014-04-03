127a5da6cd8a72452065a16c5d031c24ccd2c2d2
Scott Ringwelski <sgringwe@mtu.edu>
2014-04-03 17:22:29 -0400

    add a second file just to test it out

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'handshake'
-copyright = u'2014, Scott Ringwelski'
+copyright = u'2014, Stryder Corp'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
